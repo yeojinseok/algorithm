5a3c82dfe59097d0a63199901c0cd0bcbf4e47aa
jinseok <yeojinseuk@imcloud.co.kr>
2022-06-13 23:50:12 +0900

    6/13일

--- api.js
+++ api.js
@@ -2,25 +2,48 @@ const express = require('express')
 const app = express()
 
 const server = app.listen(3001, () => {
-  let str = 'BACBACCACCBDEDE'
+  let str = 'AbaAeCe'
+  let str2 = 'baeeACA'
 
-  console.log(solution(str))
+  console.log(solution(str, str2))
 
-  function solution(str) {
-    let answer
-    let st = new Map()
-    for (let x of str) {
-      if (st.has(x)) st.set(x, st.get(x) + 1)
-      else st.set(x, 1)
+  // function solution(str1, str2) {  내가 푼 답이지만 옳지못함.
+  //   let answer = ''
+  //   const strMap1 = new Map()
+  //   const strMap2 = new Map()
+  //   for (let x of str1) {
+  //     if (strMap1.has(x)) strMap1.set(x, strMap1.get(x) + 1)
+  //     else strMap1.set(x, 1)
+  //   }
+  //   for (let x of str2) {
+  //     if (strMap2.has(x)) strMap2.set(x, strMap2.get(x) + 1)
+  //     else strMap2.set(x, 1)
+  //   }
+  //   for (let [key, value] of strMap1) {
+  //     if (!strMap2.has(key)) {
+  //       answer = 'NO'
+  //       break
+  //     } else if (strMap1.get(key) == strMap2.get(key)) {
+  //       console.log(strMap1.get(key), strMap2.get(key))
+  //       answer = 'YES'
+  //     } else {
+  //       answer = 'NO'
+  //       break
+  //     }
+  //   }
+  //   return answer
+  // }
+  function solution(str1, str2) {
+    let answer = 'YES'
+    const strMap1 = new Map()
+    for (let x of str1) {
+      if (strMap1.has(x)) strMap1.set(x, strMap1.get(x) + 1)
+      else strMap1.set(x, 1)
     }
-    let max = Number.MIN_SAFE_INTEGER
-    for (let [key, val] of st) {
-      if (max < val) {
-        max = val
-        answer = key
-      }
+    for (let x of str2) {
+      if (!strMap1.has(x) || strMap1.get(x) == 0) return 'NO'
+      strMap1.set(x, strMap1.get(x) - 1)
     }
-
     return answer
   }
 })
