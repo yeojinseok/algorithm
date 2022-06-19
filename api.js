ecdb81ea675caefffc73b1811d5dad544ab91a47
jinseok <yeojinseuk@imcloud.co.kr>
2022-06-19 17:14:07 +0900

    6/18

--- api.js
+++ api.js
@@ -2,48 +2,20 @@ const express = require('express')
 const app = express()
 
 const server = app.listen(3001, () => {
-  let str = 'AbaAeCe'
-  let str2 = 'baeeACA'
+  let str = 'CBA'
+  let str2 = 'CBDAGE'
 
   console.log(solution(str, str2))
-
-  // function solution(str1, str2) {  내가 푼 답이지만 옳지못함.
-  //   let answer = ''
-  //   const strMap1 = new Map()
-  //   const strMap2 = new Map()
-  //   for (let x of str1) {
-  //     if (strMap1.has(x)) strMap1.set(x, strMap1.get(x) + 1)
-  //     else strMap1.set(x, 1)
-  //   }
-  //   for (let x of str2) {
-  //     if (strMap2.has(x)) strMap2.set(x, strMap2.get(x) + 1)
-  //     else strMap2.set(x, 1)
-  //   }
-  //   for (let [key, value] of strMap1) {
-  //     if (!strMap2.has(key)) {
-  //       answer = 'NO'
-  //       break
-  //     } else if (strMap1.get(key) == strMap2.get(key)) {
-  //       console.log(strMap1.get(key), strMap2.get(key))
-  //       answer = 'YES'
-  //     } else {
-  //       answer = 'NO'
-  //       break
-  //     }
-  //   }
-  //   return answer
-  // }
-  function solution(str1, str2) {
-    let answer = 'YES'
-    const strMap1 = new Map()
-    for (let x of str1) {
-      if (strMap1.has(x)) strMap1.set(x, strMap1.get(x) + 1)
-      else strMap1.set(x, 1)
+  function solution(must, dolist) {
+    let answer
+    let mustList = new Set(must)
+    let checkList = ''
+    for (let i = 0; i < dolist.length; i++) {
+      if (mustList.has(dolist[i])) checkList = checkList + dolist[i]
     }
-    for (let x of str2) {
-      if (!strMap1.has(x) || strMap1.get(x) == 0) return 'NO'
-      strMap1.set(x, strMap1.get(x) - 1)
-    }
+    if (checkList == must) {
+      answer = 'YES'
+    } else answer = 'NO'
     return answer
   }
 })
